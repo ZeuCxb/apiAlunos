a98d0851674f786c2ceed7a4f370ee8156808610
Eliseu Benedito Codinhoto <zeucxb@gmail.com>
2015-10-19 14:55:52 -0200

    port correction

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express')
 	, app = express()
 	, server = require('http').Server(app);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
@@ -14,6 +16,6 @@ load('models')
 	.then('routes')
 	.into(app);
 
-server.listen(5000, function() {
+server.listen(app.get('port'), function() {
 	console.log('Server online.');
 });
